f91fa78b6129699b04bf63a45ea0ae530d27f1ab
Matt Friedman <maf675@gmail.com>
2015-05-30 18:31:03 -0700

    Require 3.2-dev in the ext file as well

--- ext.php
+++ ext.php
@@ -12,17 +12,14 @@ namespace vse\abbc3;
 
 class ext extends \phpbb\extension\base
 {
-	/** @var string Require 3.1.3 due to throwing new exceptions
-	 *              and using containerAware migration files.
-	 */
-	const PHPBB_VERSION = '3.1.3';
+	/** @var string Require 3.2.0-dev due to new faq controller route for bbcodes */
+	const PHPBB_MIN_VERSION = '3.2.0-dev';
 
 	/**
 	 * {@inheritdoc}
 	 */
 	public function is_enableable()
 	{
-		$config = $this->container->get('config');
-		return phpbb_version_compare($config['version'], self::PHPBB_VERSION, '>=');
+		return phpbb_version_compare(PHPBB_VERSION, self::PHPBB_MIN_VERSION, '>=');
 	}
 }
